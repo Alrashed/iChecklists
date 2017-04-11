d083e4162eda319ac2c0d27ce9d09887339ed92f
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-04-11 10:05:38 -0700

    First commit

---
+++ iChecklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iChecklists
+//
+//  Created by Khalid Alrashed on 4/11/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
