8e9aad1c3a515958fdb61a5b71229463bf1ba9ac
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-04-11 16:44:18 -0700

    Create ItemDetailViewController and its storyboard view

---
+++ iChecklists/ItemDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItemDetailViewController.swift
+//  iChecklists
+//
+//  Created by Khalid Alrashed on 4/11/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
