d8deda9fa7b3382f8a2e60839e57ca7eded24604
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-04-11 10:30:51 -0700

    Create ChecklistViewController and its storyboard table view scene

---
+++ iChecklists/ChecklistViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ChecklistViewController.swift
+//  iChecklists
+//
+//  Created by Khalid Alrashed on 4/11/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        return cell
+    }
+}
