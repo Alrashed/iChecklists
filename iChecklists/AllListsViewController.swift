f1587135c7046706463642bb404124c3e24fdf6f
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-04-11 20:18:12 -0700

    Create AllListsViewController and its storyboard

---
+++ iChecklists/AllListsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AllListsViewController.swift
+//  iChecklists
+//
+//  Created by Khalid Alrashed on 4/11/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func makeCell(for tableView: UITableView) -> UITableViewCell {
+        let cellIdentifer = "Cell"
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifer) {
+            return cell
+        } else {
+            return UITableViewCell(style: .default, reuseIdentifier: cellIdentifer)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = makeCell(for: tableView)
+        cell.textLabel!.text = "List \(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+    }
+}
