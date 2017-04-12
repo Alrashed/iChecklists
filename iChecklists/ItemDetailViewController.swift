4a8449ae23ecc413aaee26375557331147fe6f03
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-04-11 17:26:20 -0700

    Validate input of text field

--- iChecklists/ItemDetailViewController.swift
+++ iChecklists/ItemDetailViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ItemDetailViewController: UITableViewController {
+class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,5 +34,14 @@ class ItemDetailViewController: UITableViewController {
         print("Content of text field: \(textField.text!)")
         
         dismiss(animated: true, completion: nil)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneBarButton.isEnabled = (newText.length > 0)
+        
+        return true
     }
 }
